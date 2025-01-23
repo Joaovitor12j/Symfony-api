15e9567479617c934cc5263fe1f683af067a46b2
Joao Vitor <joaovitor12j@gmail.com>
2025-01-23 01:45:12 -0300

    feat(front-end): Profile button added
    Profile button added

--- front-end/src/views/UserRegistered.vue
+++ front-end/src/views/UserRegistered.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <h1>Olá, {{ user.name }}! O seu cadastro foi realizado com sucesso!</h1>
+    <button id="profile" @click="goToProfile">Acessar Perfil</button>
     <button @click="logout">Sair</button>
+    <div v-if="errors.message" class="erro">{{ errors.message }}</div>
   </div>
-  <div v-if="errors.message" class="erro">{{ errors.message }}</div>
 </template>
 
 <script lang="ts">
@@ -11,13 +12,13 @@ import { defineComponent, ref, onMounted } from 'vue';
 import api from '@/api';
 import { useStore } from 'vuex';
 import { User } from '@/models/User';
-import type {Errors} from "@/models/Erorrs.ts";
+import type { Errors } from '@/models/Erorrs.ts';
 
 export default defineComponent({
   name: 'UserRegistered',
   setup() {
-    const user = ref<User>({ name: '', email: ''});
-    const errors = ref<Errors>({message: ''});
+    const user = ref<User>({ name: '', email: '' });
+    const errors = ref<Errors>({ message: '' });
     const store = useStore();
 
     onMounted(async () => {
@@ -26,11 +27,15 @@ export default defineComponent({
         user.value = response.data;
       } catch (error: any) {
         if (error.response) {
-          errors.message = error.response.data.mensagem;
+          errors.value.message = error.response.data.mensagem;
           window.location.href = '/login';
         }
       }
     });
+
+    const goToProfile = () => {
+      window.location.href = '/user';
+    };
 
     const logout = async () => {
       try {
@@ -45,6 +50,7 @@ export default defineComponent({
     return {
       user,
       errors,
+      goToProfile,
       logout
     };
   },
@@ -68,5 +74,8 @@ button {
 }
 button:hover {
   background-color: #38a274;
+}
+#profile {
+  margin: 20px;
 }
 </style>
