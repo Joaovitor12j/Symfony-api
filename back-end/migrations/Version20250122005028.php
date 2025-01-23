c7691e9dd0fc89e348dfa4ff40f5308654ba1474
Joao Vitor <joaovitor12j@gmail.com>
2025-01-23 01:52:33 -0300

    style(back-end): Adjusts
    Migration

--- back-end/migrations/Version20250122005028.php
+++ back-end/migrations/Version20250122005028.php
@@ -7,25 +7,30 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20250122005028 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Criação da tabela user com campos básicos: id, name, email, password e created_at';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, create_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('
+            CREATE TABLE user (
+                id INT AUTO_INCREMENT NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                email VARCHAR(255) NOT NULL UNIQUE,
+                password VARCHAR(255) NOT NULL,
+                created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                PRIMARY KEY(id)
+            ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        ');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
+        // Remove a tabela user
         $this->addSql('DROP TABLE user');
     }
 }
